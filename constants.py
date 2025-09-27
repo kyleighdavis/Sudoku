efe001be40fc4ea2935992c07107f83e508617e5
kyleighdavis <krdfl@yahoo.com>
2025-09-26 22:27:43 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,29 @@
+#screen constants
+WIDTH = 660
+HEIGHT = WIDTH + (WIDTH // 6)
+
+#constants for start screen
+fsBackColor = (186, 255, 201)
+startButtonColors = (255, 255, 186)
+darkButtonCol = (245, 245, 182)
+buttonTextCol = (139, 128, 0)
+recHeight = HEIGHT // 10
+recWidth = WIDTH * 3 // 11
+startTitleText = (255, 182, 193)
+
+#constant for sudoku board
+gameBackColor = (255, 223, 225)
+gridColor = (255, 105, 180)
+cellHeight = (WIDTH - (WIDTH // 10)) // 9
+cellWidth = WIDTH // 9
+thickLine = 5
+thinLine = thickLine//2
+sudokuButtonColor = (255, 255, 186)
+ButtonHighLight = (193, 225, 193)
+sButtonTxtColor = (0, 128, 0)
+gridRed = (255, 0, 0)
+ogNumColor = (255, 105, 180)
+enterNumColor = (245, 172, 183)
+sketchNumColor = (175, 120, 186)
+numSizeBig = WIDTH // 10
+numSizeSmall = WIDTH // 15
